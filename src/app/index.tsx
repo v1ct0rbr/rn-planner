d8b8fd743aadfdaab7940fb6ebf8e01fd484c737
Victor Queiroga <victorqueiroga.jp@gmail.com>
2024-07-08 22:39:31 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex align-middle justify-center">
+        <Text className="font-bold">App</Text>
+    </View>
+  );
+}
